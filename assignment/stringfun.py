a3a54dea8fe3f6b763635ea55d13125d55480894
vinothkumarbv <37235499+vinothkumarbv@users.noreply.github.com>
2018-03-10 15:17:24 +0530

    Add files via upload

---
+++ assignment/stringfun.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+a='Programmer'
+print (a.count('r'))
+print (a.find('r'))
+print (a.replace('m','y'))
+if __name__ == '__main__':
+    main()
