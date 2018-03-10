984be8f5e380d1b608fbc78199380713f8a2e1c6
vinothkumarbv <37235499+vinothkumarbv@users.noreply.github.com>
2018-03-10 16:08:42 +0530

    Add files via upload

---
+++ assignment/age.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+a=int(raw_input("Please enter an integer: "))
+if(a>=20):
+ print('eligible')
+elif(a<20):
+ print('not eligible')
